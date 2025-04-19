96af01c52f4b6196afca611769ee8f8f2700a029
Arnob Pandit <142673119+Arnob004@users.noreply.github.com>
2025-04-19 23:34:10 +0530

    Update ProtectRoute.js

--- controller/ProtectRoute.js
+++ controller/ProtectRoute.js
@@ -6,14 +6,14 @@ export const ProtectRoute = async (req, res, next) => {
     const token = req.cookies.token;
     if (!token) return res.status(401).json({ message: "No token" });
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET || "token");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userid);
 
-    req.user = await User.findById(decoded.userid);
-    if (!req.user) return res.status(404).json({ message: "User not found" });
+    if (!user) return res.status(404).json({ message: "User not found" });
 
+    req.user = user;
     next();
   } catch (err) {
-    console.error("ProtectRoute Error:", err);
     res.status(401).json({ message: "Not authorized" });
   }
 };
